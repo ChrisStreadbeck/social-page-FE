7b3765c9b286171f6e1dabcbb38e89dc4c1696ab
jessebettridge <jessebett92@gmail.com>
2019-08-05 14:32:56 -0600

    added navbar / footer

--- src/components/app.js
+++ src/components/app.js
@@ -2,17 +2,21 @@ import React, { Component } from "react";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import NavBar from "./navigation/navBar";
+import Footer from "./navigation/footer";
 import Home from "./pages/home";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
+        <NavBar />
         <Router>
           <Switch>
-            <Route exact to="/" Component={Home} />
+            <Route exact path="/" component={Home} />
           </Switch>
         </Router>
+        <Footer />
       </div>
     );
   }
