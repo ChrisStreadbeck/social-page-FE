3627528471f9438e84527fbaf752d399462a6cea
jessebettridge <jessebett92@gmail.com>
2019-08-05 11:38:08 -0600

    changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 
-export default class App extends Component {
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "./pages/home";
+
+class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>DevCamp</h1>
-        <h2>React Redux</h2>
+        <Router>
+          <Switch>
+            <Route exact to="/" Component={Home} />
+          </Switch>
+        </Router>
       </div>
     );
   }
 }
+
+export default App;
