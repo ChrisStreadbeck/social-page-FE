313601a9d5b8c2050533872b1220c034281a1d37
jessebettridge <jessebett92@gmail.com>
2019-08-06 11:46:08 -0600

    added CardDetail

--- src/components/app.js
+++ src/components/app.js
@@ -4,22 +4,24 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./navigation/navBar";
 import Footer from "./navigation/footer";
 import Home from "./pages/home";
+import NoMatch from "./pages/no-match";
 import CardFrame from "./cards/cardFrame";
+import CardDetail from "./cards/cardDetail";
 
 class App extends Component {
   render() {
     return (
-      <div className="app">   
-
+      <div className="app">
         <NavBar />
         <CardFrame />
         <Router>
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/c/:slug" component={CardDetail} />
+            <Route component={NoMatch} />
           </Switch>
         </Router>
         <Footer />
-
       </div>
     );
   }
