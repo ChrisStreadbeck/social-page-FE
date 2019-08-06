f3580a6c69643044a467f02610f966ad6ad004ba
Ethan Deans <deanseth0000@gmail.com>
2019-08-06 09:28:47 -0600

    fiddled around with styles

--- src/components/app.js
+++ src/components/app.js
@@ -4,8 +4,31 @@ export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>DevCamp</h1>
-        <h2>React Redux</h2>
+        <div className="navbar">
+          <div className="logo">
+            <h1>Logo</h1>
+          </div>
+          <div className="nav-links">
+            <div className="nav1">
+              <h1>nav link</h1>
+            </div>
+            <div className="nav1">
+              <h1>nav link</h1>
+            </div>
+            <div className="nav1">
+              <h1>nav link</h1>
+            </div>
+            <div className="nav1">
+              <h1>nav link</h1>
+            </div>
+          </div>
+        </div>
+        <div className="middle">
+          <h1>this is where the cards go</h1>
+        </div>
+        <div className="footer">
+          <h1>footer</h1>
+        </div>
       </div>
     );
   }
