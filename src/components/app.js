5f3198583b6f43aaec98a97cd9b9c5144dec4183
gabrielaMendez <ella.mendez1127@gmail.com>
2019-08-06 10:10:32 -0500

    profile cards skeleton

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./pages/home";
+import CardFrame from "./cards/cardFrame";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
+        <CardFrame />
         <Router>
           <Switch>
             <Route exact to="/" Component={Home} />
