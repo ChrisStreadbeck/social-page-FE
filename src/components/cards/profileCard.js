3a4b0556641a28b905b35b2accdb07fc41d9257e
ChrisStreadbeck <han0987@gmail.com>
2019-08-07 13:16:44 -0600

    refresh page

--- src/components/cards/profileCard.js
+++ src/components/cards/profileCard.js
@@ -14,6 +14,7 @@ const ProfileCard = props => {
     })
       .then(setSocial(socials.filter(social => social.id !== id)))
       .then(console.log("Deleted!"))
+      .then(window.location.reload())
       .catch(error => console.log("deletion error", error));
   };
 
