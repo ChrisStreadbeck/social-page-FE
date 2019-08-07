063fda04a30ac043789b5fed2fa29e942d683bb9
gabrielaMendez <ella.mendez1127@gmail.com>
2019-08-07 00:02:58 -0500

    API data updated for cards

--- src/components/cards/cardFrame.js
+++ src/components/cards/cardFrame.js
@@ -1,55 +1,50 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import ProfileCard from "./profileCard";
 
 class CardFrame extends Component {
   constructor() {
     super();
+
+    this.state = {
+      isLoading: false,
+      data: []
+    };
+
+    this.getProfileItems = this.getProfileItems.bind(this);
+  }
+
+  getProfileItems() {
+    axios
+      .get("https://social-page-be.herokuapp.com/socials")
+      .then(response => {
+        this.setState({
+          data: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   renderUsers() {
-    const people = [
-      {
-        id: 1,
-        image:
-          "https://vignette.wikia.nocookie.net/arresteddevelopment/images/e/e6/3x3_Bob_Loblaw.png/revision/latest?cb=20121117203508",
-        longdescription:
-          "You are smart. This aint your first rodeo, and you are not about to fall for the preposterous claims made by so many of the profiles on this site. So heres a refreshing perspective—the truth.I pay my mortgage. I wear socks that match. I am an honest man, with a decent career and strong values. So While I could regale you with stories of my trips to Paris or how I resemble Ryan Gosling…I know that good communication is a foundation for every relationship. So if we are on the same wavelength, read on…",
-        name: "Bob Loblaw",
-        shortdescription: "I, Bob, like to take long walks on the beach..."
-      },
-      {
-        id: 2,
-        image:
-          "https://vignette.wikia.nocookie.net/arresteddevelopment/images/e/e6/3x3_Bob_Loblaw.png/revision/latest?cb=20121117203508",
-        longdescription:
-          "You are smart. This aint your first rodeo, and you are not about to fall for the preposterous claims made by so many of the profiles on this site. So heres a refreshing perspective—the truth.I pay my mortgage. I wear socks that match. I am an honest man, with a decent career and strong values. So While I could regale you with stories of my trips to Paris or how I resemble Ryan Gosling…I know that good communication is a foundation for every relationship. So if we are on the same wavelength, read on…",
-        name: "Bob Loblaw",
-        shortdescription: "I, Bob, like to take long walks on the beach..."
-      },
-      {
-        id: 3,
-        image:
-          "https://res.cloudinary.com/cstread/image/upload/v1565121532/u5fiacpzralalrpdty3y.png",
-        longdescription:
-          "Here go Lots of words to fill in his long bio with more information.  There are two sentences.  Oh wait, three...",
-        name: "Stuart Hargrave",
-        shortdescription: "Super Normal Man "
-      }
-    ];
-
-    return people.map(user => {
+    return this.state.data.map(user => {
       return (
         <ProfileCard
+          key={user.id}
           image={user.image}
           person={user.name}
           tag={user.shortdescription}
           bio={user.longdescription}
-
-          slug={user.slug}
+          slug={user.id}
         />
       );
     });
+  }
+
+  componentDidMount() {
+    this.getProfileItems();
   }
 
   render() {
--- src/components/cards/profileCard.js
+++ src/components/cards/profileCard.js
@@ -5,12 +5,11 @@ const ProfileCard = props => {
   return (
     <div className="profile-card-wrapper">
       <div className="profile-card">
-
         <img src={props.image} alt="" />
         <div className="profile-card-body">
           <h2>{props.person}</h2>
           <h3>{props.tag}</h3>
-          <p>{props.bio}</p>  
+          <p>{props.bio}</p>
           <Link to={`/c/${props.slug}`}>
             <button>Button</button>
           </Link>
